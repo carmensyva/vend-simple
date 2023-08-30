569bafe2238369501f3e83c3eaee235764335f0d
carmensyva <81755181+carmensyva@users.noreply.github.com>
2023-08-30 22:33:01 +0700

    Add files via upload

---
+++ docker_entrypoint.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+CURRENT_USER=$(whoami)
+echo "Current user: $CURRENT_USER"
+
+echo "*********************"
+echo "** Verify enviroment values"
+echo "*********************"
+
+# cloudant
+echo "${CLOUDANT_USERNAME}"
+echo "${CLOUDANT_PASSWORD}"
+echo "${CLOUDANT_URL}"
+echo "${CLOUDANT_PORT}"
+echo "${CLOUDANT_NAME}"
+# application
+echo "${VEND_USAGE}"
+echo "${USER}"
+echo "${USERPASSWORD}"
+echo "${ADMINUSER}"
+echo "${ADMINUSERPASSWORD}"
+
+echo "*********************"
+echo "** Create enviroment file "
+echo "*********************"
+
+"/bin/sh" ./generate_env-config.sh > ./.env
+more .env
+
+echo "*********************"
+echo "** Start server"
+echo "*********************"
+
+ls 
+
+node server.js
+echo "npm start - doesn't work at the moment on openshift"
+
